ef67caefccbd6c452248d7c27426f0e611579dd7
vdegenne <vdegenne@gmail.com>
2017-08-03 17:40:02 +0200

    add the code file

---
+++ src/themath.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function addition (a, b) {
+  return a + b;
+}
+
+function substraction (a, b) {
+  return a - b;
+}
+
+function multiplication (a, b) {
+  return a * b;
+}
+
+function division (a, b) {
+  return a / b;
+}
